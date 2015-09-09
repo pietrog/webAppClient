6ae72fd2167f25ee1fe182ba18eeed1124d48dea
Pierre Gaulard <pierre.gaulard@gmail.com>
2015-09-09 19:16:04 +0200

    *File structure* Move unit test files to their own folder in app/components

--- app/components/version/tests/unit/version_test.js
+++ app/components/version/tests/unit/version_test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-describe('myApp.version module', function() {
-  beforeEach(module('myApp.version'));
+describe('parap.version module', function() {
+  beforeEach(module('parap.version'));
 
   describe('version service', function() {
     it('should return current version', inject(function(version) {
