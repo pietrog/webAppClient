72e64eb9015639454574701bf365d4afbf6f47ca
pietroXPS <pierre.gaulard@gmail.com>
2015-09-29 22:21:09 +0200

    User component changes for new routing

--- app/components/user/config/userConfig.js
+++ app/components/user/config/userConfig.js
@@ -7,28 +7,39 @@
     
     function UserConfig($stateProvider){
 	$stateProvider
-	    .state('user-login', {
+	    .state('root.user-login', {
 		url: "/login",
-		templateUrl: "components/user/views/login.html",
-		controller: 'UserLoginCtrl',
-		controllerAs: 'userCtrl'
-
+		views:{
+		    "main@": {
+			templateUrl: "components/user/views/login.html",
+			controller: 'UserLoginCtrl',
+			controllerAs: 'userCtrl'
+		    }
+		}
 	    })
 	    .state('user-logout', {
 		url: '/',
 		templateUrl: 'components/home/home.html'
 	    })
-	    .state('user-signup', {
+	    .state('root.user-signup', {
 		url: "/signup",
-		templateUrl: 'components/user/views/signup.html',
-		controller: 'UserCtrl',
-		controllerAs: 'userCtrl'
+		views:{
+		    "main@": {
+			templateUrl: "components/user/views/signup.html",
+			controller: 'UserCtrl',
+			controllerAs: 'userCtrl'
+		    }
+		}
 	    })
-	    .state('user-list', {
-		url: "/list",
-		templateUrl: 'components/user/views/list.html',
-		controller: 'UserCtrl',
-		controllerAs: 'userCtrl'
+	    .state('root.user-list', {
+		url: "/userlist",
+		views:{
+		    "main@": {
+			templateUrl: "components/user/views/list.html",
+			controller: 'UserCtrl',
+			controllerAs: 'userCtrl'
+		    }
+		}
 	    });
     }
 
